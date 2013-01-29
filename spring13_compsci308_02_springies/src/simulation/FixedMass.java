5625488f3942b5d073ab1b06b6e13994f54baed0
jo63 <jo63@duke.edu>
2013-01-29 18:13:41 -0500

    herro

---
+++ spring13_compsci308_02_springies/src/simulation/FixedMass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package simulation;
+
+/**
+ * @author junho
+ *
+ */
+public class FixedMass extends Mass {
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param mass
+	 */
+	public FixedMass(double x, double y, double mass) {
+		super(x, y, mass);
+		// TODO Auto-generated constructor stub
+	}
+
+}
