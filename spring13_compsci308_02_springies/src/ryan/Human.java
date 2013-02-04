746b484835865c22922844547308f7aa3c571856
Kevin Oh <jo63@duke.edu>
2013-02-04 14:19:11 -0500

    hi

---
+++ spring13_compsci308_02_springies/src/ryan/Human.java
@@ -0,0 +1,10 @@
+package ryan;
+
+public abstract class Human {
+
+	public Human() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public abstract void Slap();
+}
---
+++ spring13_compsci308_02_springies/src/ryan/Ryan.java
@@ -0,0 +1,16 @@
+package ryan;
+
+public class Ryan extends Human{
+
+	public Ryan() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Slap() {
+		System.out.println("hello");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
