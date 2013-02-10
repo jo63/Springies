f3193fba60ef1734f2382dc6a5526527b5e7bbfc
Kevin Oh <jo63@duke.edu>
2013-02-09 22:09:47 -0500

    changed folder setup

---
+++ src/simulation/Physics.java
@@ -0,0 +1,49 @@
+package simulation;
+
+import java.awt.Dimension;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import util.*;
+import physicsForces.CenterOfMass;
+import physicsForces.Force;
+import physicsForces.WallRepulsion;
+
+public class Physics {
+
+	private Map<String, Force> myForces;
+	private WallRepulsion myWall = new WallRepulsion();
+	
+	public Physics()
+	{
+		myForces = new HashMap<String, Force>();
+	}
+
+	public void addForce(String forceType, Force force)
+	{
+		myForces.put(forceType, force);
+	}
+	
+	public Map<String, Force> getForces()
+	{
+		return myForces;
+	}
+	
+	public WallRepulsion getWall()
+	{
+		return myWall;
+	}
+	public void update(List<Mass> massList, Dimension bounds)
+	{
+		if((myForces.get("centermass")) != null)
+		{
+			((CenterOfMass)(myForces.get("centermass"))).setCenterMassPosition(massList);
+		}
+		if((myForces.get("wall")) != null)
+		{
+			((WallRepulsion)(myForces.get("wall"))).setBounds(bounds);
+		}
+	}
+	
+}
