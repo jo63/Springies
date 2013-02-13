f96c9f9f0e1ce4abff9c2f2aa3c22bf95d2b2982
Kevin Oh <jo63@duke.edu>
2013-02-12 21:57:23 -0500

    Revert "added to buttonPrresses"
    This reverts commit 1f8274b174fd90115ee5877342460ab96de55800.

--- springies/src/simulation/buttonPresses.java
+++ springies/src/simulation/buttonPresses.java
@@ -1,8 +1,7 @@
 package simulation;
 
 import java.awt.event.KeyEvent;
-import java.util.HashSet;
-import java.util.Set;
+
 import javax.swing.JFileChooser;
 
 public class buttonPresses {
@@ -22,38 +21,17 @@ public class buttonPresses {
 	private static final int TOGGLE_WALL_THREE = KeyEvent.VK_3;
 	private static final int TOGGLE_WALL_FOUR = KeyEvent.VK_4;
 	
-	//private Set<Integer> myKeysPressed;
 	
-	public buttonPresses(HashSet<Integer> keysPressed)
+	
+	public buttonPresses()
 	{
-		HashSet<Integer> myKeysPressed = keysPressed;
-		performAction(myKeysPressed);
+		
 	}
 	
-	public void performAction(HashSet<Integer> keysPressed)
+	public void update()
 	{
-		for(Integer key : keysPressed)
-		{
-			loadAssembly(key);
-		}
+		
 	}
-	
-	private void loadAssembly(Integer key)
-	{
-		if(key == LOAD_ASSEMBLY)
-		{
-			loadModel();
-		}
-	}
-	
-	private void clearModel(Integer key)
-	{
-		if(key == CLEAR_ASSEMBLY)
-		{
-			//clear the assembly
-		}
-	}
-	
 	
 	private void loadModel () {
         SpriteFactory factory = new SpriteFactory();
