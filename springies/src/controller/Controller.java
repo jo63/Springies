c88d7f3e07f27c71b4aa4dfc18e89e04b26c4479
Kevin Oh <jo63@duke.edu>
2013-02-13 19:40:00 -0500

    ho

--- springies/src/controller/Controller.java
+++ springies/src/controller/Controller.java
@@ -18,10 +18,9 @@ public class Controller {
 	private static final int LOAD_ASSEMBLY = KeyEvent.VK_N;
 	private static final int CLEAR_ASSEMBLY = KeyEvent.VK_C;
 	private static final int TOGGLE_GRAVITY = KeyEvent.VK_G;
-	private static final int TOGGLE_VISCOSITY = KeyEvent.VK_V;
+	private static final int TOGGLE_VISCOSITYㄴ = KeyEvent.VK_V;
 	private static final int TOGGLE_CENTER_OF_MASS = KeyEvent.VK_M;
 	private static final int TOGGLE_WALL_ONE = KeyEvent.VK_1;
-	private static final int TOGGLE_WALL_TWO = KeyEvent.VK_2;
 	private static final int TOGGLE_WALL_THREE = KeyEvent.VK_3;
 	private static final int TOGGLE_WALL_FOUR = KeyEvent.VK_4;
 	private static final int INCREASE_UP = KeyEvent.VK_UP;
